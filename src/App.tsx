15993e339ecb6a1d6e2ee5324954ca49288ce8b3
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-06-20 07:32:04 +0900

    fea) section3실습 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Header/Main";
+
 function App() {
   return (
     <>
+      <Header/>
+        <Main/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Header/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <>
+            <h1>Main Component</h1>
+        </>
+    );
+}
+
+export default Main;
