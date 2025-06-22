a1c15940b138b1ed835cd7afdc7ca069b37122b7
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-06-23 07:36:17 +0900

    fea)[6/23] 이벤트 캡쳐링

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 function Table(props) {
     return (
         <>
-            <table border={2}  onClick={()=> console.log('table')}>
-                <tbody onClick={() => console.log('tbody')}>
+            <table border={2}  onClickCapture={()=> console.log('table')}>
+                <tbody onClickCapture={() => console.log('tbody')}>
                     {/*{props.children.map((child, index) => (*/}
-                        <tr key={1} onClick={() => console.log('tr')}>
-                            <td onClick={(event)=> {
-                                event.stopPropagation();
+                        <tr key={1} onClickCapture={() => console.log('tr')}>
+                            <td onClickCapture={(event)=> {
+                                // event.stopPropagation();
                                 console.log('td');
                             }}>MIKE</td>
                         </tr>
