e72e78d7f85e6edf2864cde528c5426b76c380a4
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-06-28 10:07:20 +0900

    fea)[6/28] 조건부 렌더링 미션

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import {useState} from "react";
+import LoginStatus from "./components/LoginV2/LoginStatus";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
-      <Header/>
-        <Main/>
-      <Footer/>
+      {/*<Header/>*/}
+      {/*  <Main/>*/}
+      {/*<Footer/>*/}
+        <LoginStatus
+            isLoggedIn={isLoggedIn}
+            handleLogin={handleLogin}
+            handleLogout={handleLogout}
+        />
     </>
   )
 }
