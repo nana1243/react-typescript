a8e6c3c6d06e2beafc346b6d998cc08022761344
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-01 07:48:31 +0900

    fea)[7/1] classname 추가 및 스타일에서 활용

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import styles from './Button.module.css';
+import classNames from "classnames/bind";
 
 function Button(props) {
+    const cx = classNames.bind(styles);
+    const isActive = false;
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         console.log(event);
@@ -10,6 +14,8 @@ function Button(props) {
     return (
         <div>
             <button onClick={(event) => handleClick(event)}>TEST</button>
+            <button className={cx('btn')}>button</button>
+            <button className={cx('btn', {'is-active' :isActive})}>Active Button</button>
         </div>
     );
 }
