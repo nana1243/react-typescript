8edaf9262e9b86e9bc81ee59c8ba464ada10a532
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-02 07:40:16 +0900

    fea)[7/2] 미션

---
+++ src/components/Button/ButtonGroup.tsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import Button from "./Button";
+import styles from "./ButtonGroup.module.css";
+
+
+function ButtonGroup(props) {
+    const initialButtons = [
+        { id: 1, label: "Button 1", icon: "🔥", isDisabled: false },
+        { id: 2, label: "Button 2", icon: "💧", isDisabled: false },
+        { id: 3, label: "Button 3", icon: "🌱", isDisabled: true },
+        { id: 4, label: "Button 4", icon: "⚡", isDisabled: false },
+    ];
+    const initialState = initialButtons.map(item => {
+        return {
+            ...item,
+            isActive: false
+        }
+    })
+
+    const [buttonsActiveList , setButtonsList] = useState(initialState)
+
+
+    const handleButtonClick = (id) => {
+        const find = buttonsActiveList.find((item) => item.id === id);
+        if (find && !find.isDisabled) {
+            const updatedButtons = buttonsActiveList.map((item) => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        isActive: !item.isActive
+                    };
+                }
+                return item;
+            });
+            setButtonsList(updatedButtons);
+        }
+    }
+    const handleReset = () => {
+        setButtonsList(initialState);
+    }
+
+
+    return (
+        <div>
+            <h1>Active {buttonsActiveList.filter(data => data.isActive)?.length}</h1>
+            <ul>
+                {initialButtons.map((item)=> {
+                    return (
+                        <>
+                            <Button
+                                key={`${item.label}_${item.id}`}
+                                id={item.id}
+                                label={item.label}
+                                icon={item.icon}
+                                isActive={buttonsActiveList.find(data => data.id === item.id).isActive}
+                                isDisabled={item.isDisabled}
+                                handleClick={handleButtonClick}
+                            />
+                        </>
+                    )
+                })}
+            </ul>
+            <button className={'button'} onClick={handleReset}>Reset</button>
+        </div>
+    );
+}
+
+export default ButtonGroup;
