f43b68c928e0dc7ae976049343c1592697b02e71
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-03 07:20:51 +0900

    fea)[7/3] styled-component 추가

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Button.module.css';
 import classNames from "classnames/bind";
+import styled from "styled-components";
 
 function Button(props) {
     const cx = classNames.bind(styles);
@@ -10,12 +11,24 @@ function Button(props) {
         console.log(event);
         console.log('Button clicked!');
     }
+    const Title = styled.h1`
+        color: blue;
+        text-decoration: underline;
+      `;
+    const Wrapper = styled.section`
+        padding: 20px;
+        border: 1px solid #ccc;
+      `;
 
     return (
         <div>
-            <button onClick={(event) => handleClick(event)}>TEST</button>
-            <button className={cx('btn')}>button</button>
-            <button className={cx('btn', {'is-active' :isActive})}>Active Button</button>
+
+            <Title>Hello, ReactJS! </Title>
+            <Wrapper>
+                <button onClick={(event) => handleClick(event)}>TEST</button>
+                <button className={cx('btn')}>button</button>
+                <button className={cx('btn', {'is-active' :isActive})}>Active Button</button>
+            </Wrapper>
         </div>
     );
 }
